702bc4e0ab62075de330797e0d0c8de4de40a6e3
EmilieGonin <emilie.yuu@gmail.com>
2022-12-15 12:59:10 +0100

    change map design

--- Resources/tileSheet-Pokemon.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tiles-pokemon" tilewidth="32" tileheight="32" tilecount="0" columns="8">
- <image source="tileSheet-Pokemon.png" width="256" height="17344"/>
-</tileset>
